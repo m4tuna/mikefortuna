64c359c0e229253a06a96f42538d2c54b9c63e17
Mike Fortuna <m4tuna@gmail.com>
2016-08-08 21:46:54 -0400

    Adding latest work.

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,20 @@
-
 $(document).ready(function() {
 
 	$("body").addClass("loaded");
 
-	$(function () {
+	var wordSwap = function() {
+		var words = Array('a creative web developer','a unicorn','a cool guy','a front end developer', 'a drummer, snowboarder and a web developer', 'not a cat', "wondering how many times you'll hit refresh to see how many alternate first sentences exist", "not the creator of this amazing background image - that would be the incredible: <a target='_blank' href='https://unsplash.com/@albertorestifo'>Alberto Restifo</a>");
+		var word = words[Math.floor(Math.random()*words.length)];
+
+		console.log("hello");
+		$('#word').append(word);
+
+	};
+
+	wordSwap();
+
+
+	$(function() {
 		$('a[rel="lightbox"]').fluidbox();
 	});
 
@@ -22,8 +33,11 @@ $(document).ready(function() {
 
 
 	$(window).scroll(function(){
-		console.log($(window).scrollTop());
-		moveHeader();
+		// console.log($(window).scrollTop());
+		// moveHeader();
+
+		$('.bg_scroll').css('background-position', 0 -0.023*$(window).scrollTop());
+
 	});
 
 
